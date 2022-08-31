e85f9719d972a86a4a33835023519be4a0d001f4
Emmanuel Rodrigues <102077709+im4nu@users.noreply.github.com>
2022-08-31 13:48:39 -0300

    Add files via upload

---
+++ scrollreveal.js
@@ -0,0 +1,7 @@
+ScrollReveal().reveal('.hero-section', { delay: 250 });
+ScrollReveal().reveal('.timeline-section', { delay: 250 });
+ScrollReveal().reveal('.timeline-img', { delay: 250 });
+ScrollReveal().reveal('.local-section', { delay: 250 });
+ScrollReveal().reveal('.sponsors', { delay: 250 });
+ScrollReveal().reveal('.footer-inf', { delay: 250 });
+ 
